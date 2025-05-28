8b80b4bb76b822461dda93e589b318c526d516fb
Parsa Black <parsagorgzan1225@gmail.com>
2025-05-29 02:52:28 +0330

    Auth

--- game/views.py
+++ game/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# game/views.py
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer, ProfileSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class ProfileView(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ProfileSerializer
+
+    def get_object(self):
+        return self.request.user
